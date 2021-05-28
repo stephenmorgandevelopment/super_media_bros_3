ab4a441b04416d527747defc72be50ce127022dd
Stephen Morgan <stephenmorgandevelopment@gmail.com>
2021-05-27 23:31:11 -0500

    initial push with flutter project setup, arch. skeleton, and partial widget creation.

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/devin-bro/sdks/flutter"
+export "FLUTTER_APPLICATION_PATH=/mnt/8b1add8f-64e5-4be3-a68a-f43de47d29c6/and-stud-projs/super_media_bros_3"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
